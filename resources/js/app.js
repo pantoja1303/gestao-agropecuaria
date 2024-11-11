60a3e25dee004637ced833e266cccfd66c093e2c
Ryan Pantoja <ryanp.pantoja@gmail.com>
2024-11-11 18:50:45 -0300

    feat: :sparkles: Melhoria no menu lateral

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,14 +5,3 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
-
-// Script para fazer as linhas clicáveis
-document.addEventListener('DOMContentLoaded', function () {
-    var rows = document.querySelectorAll('.clickable-row');
-    rows.forEach(function(row) {
-        row.addEventListener('click', function() {
-            window.open(row.dataset.href, '_blank', 'width=800,height=600');
-        });
-    });
-    });
-    
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,8 +4,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{$title}} - Gestão Agropecuária</title>
-    @vite(['resources/js/app.js', 'resources/css/app.scss'])
-    <script src="{{ asset('js/app.js') }}"></script>
+    @vite([
+        'resources/js/app.js',
+        'resources/css/app.scss',
+        'resources/js/layouts/medication.js'
+    ])
+    <style>
+/  /* Estilo para o botão com imagem e texto */
+        .nav-link.button-with-icon {
+            display: flex;
+            align-items: center;
+            padding: 8px 12px;
+            color: #fff;
+            background-color: #007bff; /* Ajuste a cor conforme necessário */
+            border-radius: 4px;
+            text-decoration: none;
+        }
+
+        .nav-link.button-with-icon img {
+            width: 24px; /* Ajuste o tamanho da imagem conforme necessário */
+            height: auto;
+            margin-right: 8px; /* Espaço entre a imagem e o texto */
+        }
+
+        .nav-link.button-with-icon:hover {
+            background-color: #0056b3; /* Cor de fundo ao passar o mouse */
+        }
+</style>
 </head> 
 <body>
     <!-- Menu lateral -->
@@ -13,19 +38,37 @@
         <div class="container-fluid">
             <ul class="navbar-nav flex-column">
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('animals.index') }}">Cadastro de Animais</a>
+                    <a class="nav-link button-with-icon" href="{{ route('dashboard.index') }}">
+                        <img src="{{ asset('images/touro.png') }}" alt="Touro">
+                        Gestão Agropecuária
+                    </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('medications.index') }}">Medicamento</a>
+                    <a class="nav-link button-with-icon" href="{{ route('animals.index') }}">
+                        <img src="{{ asset('images/silhueta-de-vaca.png') }}" alt="Silhueta de Vaca">
+                        Cadastro de Animais
+                    </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Inseminação</a>
+                    <a class="nav-link button-with-icon" href="{{ route('medications.index') }}">
+                        <img src="{{ asset('images/vacina.png') }}" alt="Vacina">
+                        Medicamento
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link button-with-icon" href="#">
+                        <img src="{{ asset('images/inseminacao.png') }}" alt="Inseminação">
+                        Inseminação
+                    </a>
                 </li>
                 <li class="nav-item mt-3">
                     <!-- Botão de Logout -->
                     <form method="POST" action="{{ route('logout') }}">
                         @csrf
-                        <button type="submit" class="btn btn-link nav-link">Logout</button>
+                        <button type="submit" class="nav-link button-with-icon">
+                            <img src="{{ asset('images/sair-do-usuario.png') }}" alt="Inseminação">
+                            Logout
+                        </button>
                     </form>
                 </li>
             </ul>
@@ -49,3 +92,16 @@
     </div>
 </body>
 </html>
+
+<script>
+// Script para fazer as linhas clicáveis
+document.addEventListener('DOMContentLoaded', function () {
+    var rows = document.querySelectorAll('.clickable-row');
+    rows.forEach(function(row) {
+        row.addEventListener('click', function() {
+            window.open(row.dataset.href, '_blank', 'width=800,height=600');
+        });
+    });
+    });
+    
+</script>
